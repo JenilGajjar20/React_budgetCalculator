04ee25885e71359cb6d958228b0ff99a0a8a54f6
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2022-10-17 03:56:39 +0530

    fix: list CSS feat: expense mark on List Item

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { MdDelete, MdEdit } from "react-icons/md";
+import React, { useEffect } from "react";
+// import { MdDelete, MdEdit } from "react-icons/md";
+import { AiFillDelete, AiTwotoneEdit } from "react-icons/ai";
 import { FaRupeeSign } from "react-icons/fa";
 
 const ExpenseItem = ({ expense, deleteItem, editItem }) => {
   const { id, expenseName, expenseAmount } = expense;
+  const [color, setColor] = React.useState("");
+  useEffect(() => {
+    if (expenseAmount > 10000) {
+      setColor("red");
+    } else {
+      setColor("#00FF00");
+    }
+  }, [expenseAmount]);
   return (
     <>
-      <li className="list__item">
+      <li
+        className="list__item"
+        style={{
+          borderLeft: `10px solid ${color}`,
+        }}
+      >
         <div className="item__info">
           <span className="item__name">{expenseName}</span>
           <span className="item__amount">
@@ -15,10 +29,10 @@ const ExpenseItem = ({ expense, deleteItem, editItem }) => {
         </div>
         <div>
           <button className="edit__btn" onClick={() => editItem(id)}>
-            <MdEdit />
+            <AiTwotoneEdit />
           </button>
           <button className="delete__btn" onClick={() => deleteItem(id)}>
-            <MdDelete />
+            <AiFillDelete />
           </button>
         </div>
       </li>
